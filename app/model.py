a8ce088b120b8935ea7d3bd9b89e0eca82813f3e
yijunxu <yijunx@umich.edu>
2022-05-29 08:42:48 +0000

    added devcontainer

---
+++ app/model.py
@@ -0,0 +1,5 @@
+# domain model
+
+# The domain model is the mental map that business owners have of their businesses.
+# All business people have these mental maps—​they’re how humans think about complex processes.
+
